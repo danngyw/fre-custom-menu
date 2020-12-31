4d99196f1366f0297b25dc239478e414b08bae3f
Dan Ngyen <wpcodev@gmail.com>
2020-12-31 15:20:46 +0700

    update method name

--- fre-custom-menu.php
+++ fre-custom-menu.php
@@ -17,7 +17,7 @@ Class Fre_Custom_Menu{
 	function __construct(){
 		require_once FRE_CUSTOM_MENU_PATH  . '/functions.php';
     	add_action( 'after_setup_theme', array($this,'fre_register_nav_menu')  );
-        add_action( 'wp_enqueue_scripts', array($this, 'mje_request_scripts') );
+        add_action( 'wp_enqueue_scripts', array($this, 'fre_custom_menu_scripts') );
 	}
 	function fre_register_nav_menu(){
         register_nav_menus( array(
@@ -26,8 +26,8 @@ Class Fre_Custom_Menu{
             'employer_menu' => __( 'Employer Header Menu(Fre Custom Menu)', 'enginethemes' ),
         ) );
     }
-    function mje_request_scripts(){
-        wp_enqueue_style( 'mje-request-css', FRE_CUSTOM_MENU_URL.'/fre_custom_menu.css',  FRE_CUSTOM_MENU_VERSION );
+    function fre_custom_menu_scripts(){
+        wp_enqueue_style( 'fre-custom-menu-css', FRE_CUSTOM_MENU_URL.'/fre_custom_menu.css',  FRE_CUSTOM_MENU_VERSION );
     }
 }
 
