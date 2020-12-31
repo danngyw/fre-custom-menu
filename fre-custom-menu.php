990dfe2bdea74ba45aceb3f279fb4543f9e8c3a0
Dan Ngyen <wpcodev@gmail.com>
2020-12-31 15:17:40 +0700

    add file

---
+++ fre-custom-menu.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Freelancer Custom Header Menu
+Plugin URI: http://EngineThemes.com/
+Description: Allow admin can easy custom the header menu.
+Version: 1.0
+Author: EngineThemes
+Author URI: http://EngineThemes.com/
+License: A "Slug" license name e.g. GPL2
+Text Domain: mje_authorize
+*/
+define( 'FRE_CUSTOM_MENU_URL', plugin_dir_url( __FILE__ ) );
+define( 'FRE_CUSTOM_MENU_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FRE_CUSTOM_MENU_VERSION', '1.1' );
+
+Class Fre_Custom_Menu{
+	function __construct(){
+		require_once FRE_CUSTOM_MENU_PATH  . '/functions.php';
+    	add_action( 'after_setup_theme', array($this,'fre_register_nav_menu')  );
+        add_action( 'wp_enqueue_scripts', array($this, 'mje_request_scripts') );
+	}
+	function fre_register_nav_menu(){
+        register_nav_menus( array(
+            'visitor_menu' => __( 'Visitor Header Menu(Fre Custom Menu)', 'text_domain' ),
+            'freelancer_menu'  => __( 'Freelancer Header Menu(Fre Custom Menu)', 'text_domain' ),
+            'employer_menu' => __( 'Employer Header Menu(Fre Custom Menu)', 'text_domain' ),
+        ) );
+    }
+    function mje_request_scripts(){
+        wp_enqueue_style( 'mje-request-css', FRE_CUSTOM_MENU_URL.'/fre_custom_menu.css',  FRE_CUSTOM_MENU_VERSION );
+    }
+}
+
+new Fre_Custom_Menu();
---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+function fre_custom_header_menu() {
+    global $user_ID;
+	$theme_location ='visitor_menu';
+
+
+    if( ! is_user_logged_in() ){
+        $theme_location = 'visitor_menu';
+    } else {
+        $user_role = ae_user_role( $user_ID );
+        if($user_role == FREELANCER){
+            $theme_location = 'freelancer_menu';
+        } else{
+            $theme_location = 'employer_menu';
+        }
+    }
+
+    if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
+    	echo '<div class="fre-menu-top">';
+        wp_nav_menu(
+        	array(
+        		'container' => 0,
+        		'theme_location' => $theme_location,
+        		'menu_class' =>'fre-menu-main',
+        	)
+        );
+        echo '</div>';
+    } else{
+    	get_template_part( 'template/header', 'menu' );
+    }
+
+}
