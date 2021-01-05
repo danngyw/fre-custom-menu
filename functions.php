a856ee3c9274d63c1116a600f7267ce997d439d6
Dan Ngyen <wpcodev@gmail.com>
2021-01-05 16:25:12 +0700

    update css

--- functions.php
+++ functions.php
@@ -16,12 +16,13 @@ function fre_custom_header_menu() {
     }
 
     if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
+        $css_class = 'fre-custom-menu  fre-user-role-'.$user_role.'-menu fre-menu-lation-'.$theme_location;
     	echo '<div class="fre-menu-top">';
         wp_nav_menu(
         	array(
         		'container' => 0,
         		'theme_location' => $theme_location,
-        		'menu_class' =>'fre-menu-main',
+        		'menu_class' =>'fre-menu-main '.$css_class,
         	)
         );
         echo '</div>';
